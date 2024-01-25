9b2ab11211778da51487bf2d4170f4dc9b6381e4
Ryan Hemlock <ryanhemlock@Ryans-Laptop.local>
2024-01-25 14:20:33 -0500

    Updated routes to match grading requirements

---
+++ controllers/thoughtController.js
@@ -0,0 +1,14 @@
+const { Thoughts, Users } = require('../models');
+
+module.exports = {
+    // TODO: Get all thoughts
+
+    // TODO: Get a single thought
+
+    // TODO: Create a new thought
+
+    // TODO: Delete an existing thought
+
+    // TODO: Update a thought
+    
+}
---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+const { ObjectId } = require('mongoose').Types;
+const { Users, Thoughts } = require('../models');
+
+module.exports = {
+    // TODO: Get all users
+
+    // TODO: Get a single user by ID
+
+    // TODO: Create a new user
+
+    // TODO: Update a single user by ID
+
+    // TODO: Delete an existing user and remove their thoughts
+
+}
